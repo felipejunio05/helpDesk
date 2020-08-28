c140c1588f8b71021b8cf01e4a54723bb5fbc338
Felipe Junio <felipe_ti@outlook.com>
2020-08-28 16:47:30 -0300

    Implementação do metodo de consulta de chamados

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -1,4 +1,18 @@
 <? require_once "validador_acesso.php"; ?>
+<?php 
+  $arquivo = fopen('arquivo.bd', 'r');
+  $chamados = [];
+
+  while( !feof($arquivo) ) {
+    $registro = fgets($arquivo);
+
+    if ( !empty($registro) ) {
+      $chamados[] = explode('::', $registro);
+    }
+  }
+
+  fclose($arquivo);
+?>
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -37,25 +51,16 @@
             </div>
             
             <div class="card-body">
-              
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
+              <? foreach($chamados as $chamado ) {?>
+                <div class="card mb-3 bg-light">
+                  <div class="card-body">
+                    <h5 class="card-title"><?= $chamado[0] ?></h5>
+                    <h6 class="card-subtitle mb-2 text-muted"><?= $chamado[1] ?></h6>
+                    <p class="card-text"><?= $chamado[2] ?></p>
 
+                  </div>
                 </div>
-              </div>
-
-              <div class="card mb-3 bg-light">
-                <div class="card-body">
-                  <h5 class="card-title">Título do chamado...</h5>
-                  <h6 class="card-subtitle mb-2 text-muted">Categoria</h6>
-                  <p class="card-text">Descrição do chamado...</p>
-
-                </div>
-              </div>
-
+              <? } ?>
               <div class="row mt-5">
                 <div class="col-6">
                   <a class="btn btn-lg btn-warning btn-block" href="home.php">Voltar</a>
